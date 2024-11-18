bc180c8243df92bd82128efa6eb4837971124fe5
Noemi Lovei <nila.noemi25@gmail.com>
2024-11-18 16:30:32 +0000

    GET info from PI

--- lateAPI.py
+++ lateAPI.py
@@ -1,8 +1,15 @@
+from flask import Flask, request
+from flask_cors import CORS
 from sense_hat import SenseHat
 import time
 
-deviceID="device1"
+deviceID="NoemisPi"
 sense = SenseHat()
+sense.clear()
+
+#create Flask app instance and apply CORS
+app = Flask(__name__)
+CORS(app)
 
 # Define colours
 green = (0, 255, 0)  
@@ -17,6 +24,23 @@ if result.tm_min > 2 < 30 or result.tm_min > 32 and result.tm_min != 0:
 
 sense.clear()  
 
+@app.route('/sensehat/environment',methods=['GET'])
+def current_environment():
+    temperature=round(sense.temperature,2)
+    humidity=round(sense.humidity,2)
+    flag="default"
+    if humidity > 90 and temperature < 20:
+     flag = "It's probably raining."
+    else:
+     flag = "The weather looks good."
+
+    msg = {"deviceID": deviceID,"temp":temperature,"humidity":humidity,"weather_conditions":flag}
+    return str(msg)+"\n"
+
+app.run(host='0.0.0.0', port=5000, debug=True)
+
+"""
+******************************
 # GET data from Pi
 def get_environmental_data():
 # Retrieve temperature and humidity
@@ -24,6 +48,7 @@ def get_environmental_data():
  humidity = sense.get_humidity()
 
  """
+"""
   if humidity > 90 and temp < 20:
     return '{"It's probably raining"}' 
  else:
@@ -31,13 +56,16 @@ def get_environmental_data():
  """
 
  # Create a dictionary
- """data = {
+"""data = {
     "deviceID": deviceID,
     "temp": round(temp, 2),
     "humidity": round(humidity, 2)
     } """
- 
+"""
  return temp, humidity
+
+********************************
+ """
  
 def check_rain_conditions(temp, humidity):
  if humidity > 90 and temp < 20:
@@ -47,9 +75,9 @@ def check_rain_conditions(temp, humidity):
 
 
 # Allow standalone testing of this module
-if __name__ == "__main__":
+#if __name__ == "__main__":
     # Example device ID and usage
-     check_rain_conditions(15,74)
+
 
 while True:
     for event in sense.stick.get_events():
